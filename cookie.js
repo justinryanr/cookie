af19974cf1cd016a5eb2aa3ad0e4010043749151
justinryanr <justin.rings1@gmail.com>
2013-02-27 12:17:15 -0800

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -20,26 +20,26 @@
 (function(){
 	window.cookie = {
 	
-			   get:   function(key) {
-			        	var i, x, y, c = document.cookie.split(";");
-				        for (i = 0; i < c.length; i++) {
-				            x = c[i].substr(0, c[i].indexOf("="));
-				            y = c[i].substr(c[i].indexOf("=") + 1);
-				            x = x.replace(/^\s+|\s+$/g, "");
-				            if (x == key) {
-				                return unescape(y);
-				            }
-				        }
-		              },
-		       set:   function(key, value, days) {
-				        var exp = new Date();
-				        exp.setDate(exp.getDate() + days);
-				        var val = escape(value) + ((days == null) ? "" : "; expires=" + exp.toUTCString());
-				        document.cookie = key + "=" + val;
-		    		  },
-    		   clear: function(key){
-				    	cookie.set(key,"",-1);
-			    	  }
+		get:   function(key) {
+			var i, x, y, c = document.cookie.split(";");
+			for (i = 0; i < c.length; i++) {
+			  x = c[i].substr(0, c[i].indexOf("="));
+			  y = c[i].substr(c[i].indexOf("=") + 1);
+			  x = x.replace(/^\s+|\s+$/g, "");
+			  if (x == key) {
+			    return unescape(y);
+			  }
+			}
+		       },
+		set:   function(key, value, days) {
+			var exp = new Date();
+			exp.setDate(exp.getDate() + days);
+			var val = escape(value) + ((days == null) ? "" : "; expires=" + exp.toUTCString());
+			document.cookie = key + "=" + val;
+		       },
+	    	clear: function(key){
+			cookie.set(key,"",-1);
+		       }
 		}
 })();
   
